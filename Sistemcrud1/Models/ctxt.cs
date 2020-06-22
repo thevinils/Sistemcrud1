b3652297d95f55833dd53715a96dc274e4c210d3
Vinicius Lima <viniciuslssilva@hotmail.com>
2020-06-22 01:30:13 -0300

    Algumas classes adicionadas e alterações feitas.

---
+++ Sistemcrud1/Models/ctxt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Sistemcrud1.Models
+{
+    public class Ctxt: DbContext 
+
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+
+    }
+}
